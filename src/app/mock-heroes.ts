97bdd650c1e3ebf1a2e8ecb351197f80f50b7522
WangTeng <wowton@qq.com>
2017-05-02 00:58:58 -0500

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero.model';
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Jon Snow'},
+  {id: 2, name: 'Arya Stark'},
+  {id: 3, name: 'Mother of Dragons'},
+  {id: 4, name: 'Emp'},
+  {id: 5, name: 'Magneta'},
+  {id: 6, name: 'RubberMan'},
+  {id: 7, name: 'Dynama'},
+  {id: 8, name: 'Dr IQ'},
+  {id: 9, name: 'Magma'},
+  {id: 10, name: 'Tornado'}
+];
