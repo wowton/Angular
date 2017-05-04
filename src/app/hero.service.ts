b512b29da7bc53cf77c916e803a57e34b12905b4
WangTeng <wowton@qq.com>
2017-05-04 13:05:18 +0800

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 
